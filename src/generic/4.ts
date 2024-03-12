32f546a097c057a10fe3a22208f2c9494336d8f3
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-03-11 21:26:33 -0400

    str

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,15 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  [key: string]: any;
+}
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<{ title: string }> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
